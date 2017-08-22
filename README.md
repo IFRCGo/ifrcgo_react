b68095a935dfecb6baf9a5c69eca37c14bdadbbb
Zibethin <nguyenththuong@gmail.com>
2017-08-22 17:20:57 +0100

    added gulp to run server and webpack at same time

--- README.md
+++ README.md
@@ -6,12 +6,28 @@ pip install django psycopg2 feedparser unicodecsv
 
 npm install
 
+# npm modules used in this project
+
+(should be listed in package.json)
+- sass
+- semantic-ui
+- babel/react
+- webpack
+- gulp
+
+
 # Set up
 
 Copy ifrc/settings.template and change as appropriate e.g. add database settings
 
-# Run
+# To start the dev server and watch file changes run:
+```
+gulp
+```
 
+If the command above doesn't work you can run the following two commands in separate windows:
+
+```
 python manage.py runserver
-
 webpack --watch
+```
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+
+/*****************************************************************
+            SET-UP
+
+******************************************************************/
+
+var gulp = require('gulp-help')(require('gulp'));
+var exec = require('child_process').exec;
+
+
+/*****************************************************************
+            GULP TASKS:
+
+    - Serving backend in dev mode
+    - watching files
+
+****************************************************************/
+
+// serving python manage.py
+gulp.task('serve:backend', function () {
+    var projDir = 'E:\\GitHub\\ifrcgo_react\\'
+    var devServerPort = process.env.PORT || 8000;
+    process.env.PYTHONUNBUFFERED = 1;
+    process.env.PYTHONDONTWRITEBITECODE = 1;
+    spawn('python', [projDir + 'manage.py', 'runserver', '127.0.0.1:' + devServerPort], {
+        stdio: 'inherit'
+    });
+});
+
+
+// Generate minified bundle -- also watches files for any changes
+gulp.task('webpack', function(cb) {
+    exec('webpack --watch', function (err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        cb(err);
+    });
+});
+
+
+gulp.task('default', function () {
+    gulp.start('webpack');
+    gulp.start('serve:backend');
+    //gulp.start('main:watch');
+});
+
